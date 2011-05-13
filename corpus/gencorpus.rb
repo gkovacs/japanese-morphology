74cee79db2d26a0628430f25cc8572500b04d01a
Geza Kovacs <gkovacs@mit.edu>
2011-05-13 15:54:13 -0400

    removed emoticon and nonsense punctuation from corpus-extracted words list

---
+++ corpus/gencorpus.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby1.9
+
+Dir.chdir("orig")
+filenames = `ls KN*`.split("\n")
+banned = []
+successful = []
+filenames.each {|fn|
+	output = `../sentextract.rb #{fn}`
+	if output.include?("===BANNED")
+		banned.push(output)
+	else
+		successful.push(output)
+	end
+}
+Dir.chdir("..")
+File.delete("extracted-raw-corpus.txt")
+File.delete("extracted-raw-corpus-banned.txt")
+File.open("extracted-raw-corpus.txt", "w") {|f|
+	f.puts(successful.join(""))
+}
+File.open("extracted-raw-corpus-banned.txt", "w") {|f|
+	f.puts(banned.join(""))
+}
+
--- corpus/sentextract.rb
+++ corpus/sentextract.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/ruby1.9
 # encoding: utf-8
 
+$LOAD_PATH << File.join(File.expand_path(File.dirname(__FILE__)), '..')
+require 'typelists.rb'
+
 fileName = ARGV[0]
 baseFileName = fileName
 if baseFileName.include?("/")
@@ -8,10 +11,9 @@ if baseFileName.include?("/")
 end
 skip = false
 skippedchars = ["*", "+", "#", "’", "　"]
-punctuation = ["。", "、"]
 words = []
 posMapping = {"名詞" => "Noun", "動詞" => "Verb", "形容詞" => "IAdjective", "指示詞" => "Demonstrative", "助動詞" => "AuxillaryVerb", "連体詞" => "PreNounAdjectival", "接尾辞" => "Suffix", "副詞" => "Adverb", "助詞" => "Particle", "判定詞" => "Decision", "接続詞" => "Conjunction", "特殊" => "Special", "接頭辞" => "Prefix", "感動詞" => "Interjection", "未定義語" => "Undefined"}
-bannedchars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "ａ", "ｂ", "ｃ", "ｄ", "ｅ", "ｆ", "ｇ", "ｈ", "ｉ", "ｊ", "ｋ", "ｌ", "ｍ", "ｎ", "ｏ", "ｐ", "ｑ", "ｒ", "ｓ", "ｔ", "ｕ", "ｖ", "ｗ", "ｘ", "ｙ", "ｚ", "Ａ", "Ｂ", "Ｃ", "Ｄ", "Ｅ", "Ｆ", "Ｇ", "Ｈ", "Ｉ", "Ｊ", "Ｋ", "Ｌ", "Ｍ", "Ｎ", "Ｏ", "Ｐ", "Ｑ", "Ｒ", "Ｓ", "Ｔ", "Ｕ", "Ｖ", "Ｗ", "Ｘ", "Ｙ", "Ｚ", "д", "∀"]
+bannedchars = romaji + ["д", "∀"]
 File.open(fileName).each { |line|
 	if line[0] == "［"
 		skip = true
@@ -40,6 +42,10 @@ File.open(fileName).each { |line|
 	end
 	words.push([conjugated, baseform, epos])
 }
+if words.length == 0
+	puts "===BANNED:LACKSWORDS #{baseFileName}"
+	exit(0)
+end
 puts "===SENTENCE:#{baseFileName}"
 puts words.map{|x| x[0]}.join("")
 puts "===WORDS:#{baseFileName}"
